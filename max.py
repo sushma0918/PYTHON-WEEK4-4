4ba34ec32e4693ff55598e7abe3a3a2bbdde446d
sushma0918 <sushmamanapuram18@gmail.com>
2026-01-06 20:39:24 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,21 @@
+#24331A05G8
+#Maximum and Minimum numbers
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+print("maximum number")
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+else:
+    print(c)
+    print("maximum number is")
+if a<b and a<c:
+    print(a)
+elif b<a and b<c:
+    print(b)
+else:
+    print(c)        
+
+
